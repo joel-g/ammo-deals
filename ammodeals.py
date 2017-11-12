7046def52b63dbb551f7f7d1ac9a70517d83323b
joel-g <joelaguerra1@gmail.com>
2017-11-12 11:58:43 -0800

    adds libraries

--- ammodeals.py
+++ ammodeals.py
@@ -1,6 +1,21 @@
 import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
-res = requests.get('https://ammoseek.com/go.to/54376990606')
-print(res)
-print(dir(res))
-print(res.text)
+
+
+# res = requests.get('https://ammoseek.com/go.to/54376990606')
+# print(res)
+# print(dir(res))
+# print(res.text)
+
+def get_deal(ammo):
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get('https://ammoseek.com/ammo/' + ammo)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    table = soup.find("table", class_="main").find("table")
+    return table
+
+
+print(get_deal('223-remington'))
